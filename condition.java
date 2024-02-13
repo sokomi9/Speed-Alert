19f8d4ee1f83f5964fa3ced61ffbcda8bdec4802
Abiud Sokomi <abiudsokomi@gmail.com>
2024-02-13 16:30:53 +0300

    my commit

---
+++ condition.java
@@ -0,0 +1,12 @@
+class Condition
+{
+    public static void main(String a[])
+    {
+        int speed = 120;
+
+        if(speed > 100)
+        System.out.println("Slow down");
+        else
+        System.out.println("You are at a manageable speed");
+    }
+}
